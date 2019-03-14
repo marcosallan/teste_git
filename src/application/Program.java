b2e47784e74f30bc52486d05ab2e5d9ca8231b9a
Marcos Allan <marcosallan@vivaldi.net>
2019-03-14 00:59:12 -0300

    Adicionado boa madrugada

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,5 +4,6 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World !");
 		System.out.println("Boa noite");
+		System.out.println("Boa madrugada");
 	}
 }
